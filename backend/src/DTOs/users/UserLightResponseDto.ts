66f6908936e628352825695d6a1fba447eda2ed4
Benjamin DUCROCQ <benjamin@ben3d.fr>
2024-09-26 20:57:10 +0200

    Ajout des coordonnées dans la liste users en recherche avancée

--- backend/src/DTOs/users/UserLightResponseDto.ts
+++ backend/src/DTOs/users/UserLightResponseDto.ts
@@ -1,12 +1,12 @@
 export interface UserLightResponseDto {
     id: number;
     username: string;
+    age: number;
     main_photo_url: string;
     location?: {
         latitude: number;
         longitude: number;
         city_name?: string;
     };
-    age: number;
     gender: number;
 }
