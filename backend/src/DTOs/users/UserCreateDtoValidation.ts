e9444b72b32bea3e4386e4d6fc7a1d30f3355825
Benjamin DUCROCQ <benjamin@ben3d.fr>
2024-12-09 23:03:27 +0100

    augmentation password.min(2) -> (5)

--- backend/src/DTOs/users/UserCreateDtoValidation.ts
+++ backend/src/DTOs/users/UserCreateDtoValidation.ts
@@ -5,5 +5,5 @@ export const UserCreateDtoValidation = joi.object({
     last_name: joi.string().required().max(255).min(1),
     first_name: joi.string().required().max(255).min(1),
     email: joi.string().email().required().max(255).min(2),
-    password: joi.string().required().max(255).min(2),
+    password: joi.string().required().max(255).min(5),
 });
